41765fe3b52c4b70c2dd81584049555302ce82de
marwa <mds08@mail.aub.edu>
2022-09-01 13:30:59 +0300

    completed assignment

---
+++ index.js/index.js
@@ -0,0 +1,77 @@
+function gameObject(){
+    home: {teamName: 'Brooklyn Nets',
+    color:['Black White'],
+    players:{
+        "Alan Anderson":{
+        number:0,
+        shoe:16,
+        points:22,
+        rebounds:12,
+        assists:12,
+        steals:3,
+        blocks:1,
+        slamDunk:1},
+        "Reggie Evans":{
+        number:30,
+        shoe:14,
+        points:12,
+        rebounds:12,
+        assists:12,
+        steals:12,
+        blocks:12,
+        slamDunk:7},
+        "Brook Lopez":{
+        number:11,
+        shoe:17,
+        points:17,
+        rebounds:19,
+        assists:10,
+        steals:3,
+        blocks:1,
+        slamDunk:15},
+        "Mason Plumlee":{
+        number:1,
+        shoe:19,
+        points:26,
+        rebounds:12,
+        assists:6,
+        steals:3,
+        blocks:8,
+        slamDunk:5},
+
+    }
+        "Jason Terry":{
+        number:31
+        shoe:15
+        points:19
+        rebounds:2
+        assists:2
+        steals:4
+        blocks:11
+        slamDunk:11},
+
+    }
+
+    away:{teamName: 'Charlotte Hornets',
+    color:['Turquoise','Purple'],
+    players:{
+        "Jeff Adrien":{
+        number:4,
+        shoe:18,
+        points:10,
+        rebounds:1,
+        assists:1,
+        steals:2,
+        blocks:7,
+        slamDunk:2}
+
+    }
+}
+}
+
+function homeTeamName() {
+    let object = gameObject()
+    return object['home']['teamName']
+  }
+
+  console.log(homeTeamName())
---
+++ src/00-objectball.js
@@ -0,0 +1,122 @@
+function gameObject(){
+    return {
+        home: {
+            teamName: 'Brooklyn Nets',
+            color: ['Black' , 'White'],
+            players: {
+                "Alan Anderson": {
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 1},
+                "Reggie Evans": {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamDunks: 7},
+                "Brook Lopez": {
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    rebounds: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 15},
+                "Mason Plumlee": {
+                    number: 1,
+                    shoe: 19,
+                    points: 26,
+                    rebounds: 12,
+                    assists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slamDunks: 5},
+             "Jason Terry": {
+        number: 31,
+        shoe: 15,
+        points: 19,
+        rebounds: 2,
+        assists: 2,
+        steals: 4,
+        blocks: 11,
+        slamDunks: 11},
+        "Reggie Evans": {
+            number: 30,
+            shoe: 14,
+            points: 12,
+            rebounds: 12,
+            assists: 12,
+            steals: 12,
+            blocks: 12,
+            slamDunks: 7},
+            "Brook Lopez": {
+            number: 11,
+            shoe: 17,
+            points: 17,
+            rebounds: 19,
+            assists: 10,
+            steals: 3,
+            blocks: 1,
+            slamDunks: 15},
+            "Mason Plumlee": {
+            number: 1,
+            shoe: 19,
+            points: 26,
+            rebounds: 12,
+            assists: 6,
+            steals: 3,
+            blocks: 8,
+            slamDunks: 5},
+        "Jason Terry": {
+            number: 31,
+            shoe: 15,
+            points: 19,
+            rebounds: 2,
+            assists: 2,
+            steals: 4,
+            blocks: 11,
+            slamDunks: 11},
+
+        }
+    },
+         away: {
+            teamName: 'Charlotte Hornets',
+            color: ['Turquoise','Purple'],
+            players: {
+            "Jeff Adrien": {
+                number: 4,
+                shoe: 18,
+                points: 10,
+                rebounds: 1,
+                assists: 1,
+                steals: 2,
+                blocks: 7,
+                slamDunks:2}
+
+    }
+}
+}
+}
+
+function numPointsScored(){
+  const game=gameObject()
+  for (const gameKey in game){
+    const teamObj = teamObbjlayers
+    debugger
+  }
+  for (const playerKey in playerObj){
+    if(playerKey===playerInput){
+      return playerObj[playerKey].points
+    }
+  }
+
+}
